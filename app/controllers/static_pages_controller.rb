99d802dd8869845d0f1a32adb0292f675082ec91
Meutel <meutel@meutel.net>
2015-06-28 11:48:20 +0200

    Add calibre database

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+	  @title = "Home"
+  end
+
+  def about
+	  @title = "About"
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'magic_multi_connections'
+
+module Calibre
+	  establish_connection :calibre
+end
