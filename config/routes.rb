ea6326612a02e1555212e3a0f98cad089927b1c5
Meutel <meutel@meutel.net>
2015-06-28 14:29:09 +0200

    show book

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  resources :books
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
