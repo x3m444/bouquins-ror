8553c371a50be117af5eb644b37dd713ab3eab74
Meutel <meutel@meutel.net>
2015-06-28 15:03:30 +0200

    fix titles

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,12 @@ class BooksController < ApplicationController
 
 	def show
 		@book = Book.find(params[:id])
+		@title = @book.title
 	end
 
 	def index
 		@books = Book.paginate(page: params[:page])
+		@title = "Books"
 	end
 
 end
