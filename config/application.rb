ef62f365c2c8811b4daed7b327ca69b6a674d986
Meutel <meutel@meutel.net>
2015-08-16 11:32:44 +0200

    fix production error

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module BouquinsRor
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-	config.web_console.whitelisted_ips = '10.8.0.0/24'
   end
 end
